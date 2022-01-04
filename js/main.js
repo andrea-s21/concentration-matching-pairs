8bf1a0db6bbd6269ecbefe61a6cc3e7b5d5b443f
andrea-s21 <andreasisneros21@gmail.com>
2022-01-03 22:36:32 -0700

    updates to html and css and initial js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 /*----- constants -----*/
+let lose = numGuesses > MAX_GUESSES;
+
+
 /*----- app's state (variables) -----*/
+let selected, shown, remove;
+
+
 /*----- cached element references -----*/
+const = 
+
+
 /*----- event listeners -----*/
+
+
+
 /*----- functions -----*/
